ae3f8e1d70b0c6356b06734dcb22103b390a3056
Mirella Di Girolamo <mirella@sciabarra.com>
2022-12-01 19:36:28 +0100

    Cookie
    Add cookies policy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+//cookies
+<script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="60149b7a-4ce0-422a-9184-c380fe6a8768" data-blockingmode="auto" type="text/javascript"></script>
+<script id="CookieDeclaration" src="https://consent.cookiebot.com/60149b7a-4ce0-422a-9184-c380fe6a8768/cd.js" type="text/javascript" async></script>
+// end declaration cookies
 <script lang="ts">
 	import Landing from './Landing.svelte';
 	import LandingMobile from './LandingMobile.svelte';
